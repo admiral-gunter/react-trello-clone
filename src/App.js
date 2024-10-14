389231ed63615949dc409bb0fd4bae2d94bc05c3
DESKTOP-EEIKLO4\fi <muhammadraflipembalap@gmail.com>
2024-10-14 22:23:09 +0700

    enhance styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState, useRef, createRef } from 'react';
 import DatePicker from "react-datepicker";
 import { format } from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
 function App() {
 
@@ -146,13 +147,14 @@ function App() {
   const [isOpen, setIsOpen] = useState({ 'sts': '', 'id': '' });
 
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: 'url("https://image.popmama.com/content-images/post/20211203/batik-mega-mendung-1jpg-60f7babef90beab9e0a08e5a3bac4964.jpg")' }}>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}
         onDragOver={handleDragOver}
       >
         {statusTasks.map((itemSts, idxSts) => (
+
           <div
-            style={{ background: itemSts.bg, width: '20%', height: '100vh', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)' }}
+            style={{ overflowY: 'auto', height: '100vh', background: itemSts.bg, width: '20%', height: '100vh', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)' }}
             onDrop={(e) => handleDrop(e, itemSts.sts)}
             id={itemSts.sts}
             key={idxSts}
@@ -161,68 +163,70 @@ function App() {
           >
 
             <div
-              style={{ fontWeight: 'bold', background: 'white', padding: '1% 0 1% 0', color: 'gray' }}
+              style={{ background: 'white', padding: '1% 0 1% 0', color: 'gray' }}
             >
               {itemSts.sts.replace('_', ' ')}
             </div>
-            {dataTasks[itemSts.sts].map((item, index) => (
-              <div
-                className={deletingTask === item.id ? 'fade-out' : 'fade-in'}
-                key={index}
-                style={{ ...TaskStyle, display: draggedElement?.id == item.id ? 'none' : 'block' }}
-                draggable
-                onDragStart={(e) => handleDragStart(e, item, itemSts.sts, index)}
-                onDragEnd={(e) => {
-                  setIsDragging(null);
-                  setDraggedElement(null)
-                }}
-                onMouseEnter={() => setTaskHoveredIndex(item.id)}
-                onMouseLeave={() => setTaskHoveredIndex(null)}
-              >
-                {taskHoveredIndex === item.id && (
-                  <span
-                    onMouseEnter={() => setDelTaskIdx(index)}
-                    onMouseLeave={() => setDelTaskIdx(null)} style={{ zIndex: '2', position: 'absolute', right: 5, top: 0 }}
-                    onClick={(e) => {
-                      setDeletingTask(item.id);
-                      setTimeout(() => {
-                        const newdataTasks = { ...dataTasks };
-                        newdataTasks[itemSts.sts].splice(index, 1);
-                        setdataTasks(newdataTasks);
-                        setDeletingTask(null);
-                      }, 500);
-                    }}
-                  >
-                    <span style={{ fontSize: 'small', fontWeight: 'bold', color: 'red', cursor: 'pointer', fontSize: delTaskIdx === index ? 'medium' : 'small' }}>Delete</span>
-                  </span>
-                )}
-                <div style={{ marginTop: '5%' }}></div>
-                <input className='form-control' onChange={(e) => chgTitle(e.target.value, itemSts.sts, index)} value={item.title} />
-                <div>
-                  <span style={{ color: 'gray' }}>Due  </span>
+            {
+              dataTasks[itemSts.sts].map((item, index) => (
+                <div
+                  className={deletingTask === item.id ? 'fade-out' : 'fade-in'}
+                  key={index}
+                  style={{ ...TaskStyle, display: draggedElement?.id == item.id ? 'none' : 'block' }}
+                  draggable
+                  onDragStart={(e) => handleDragStart(e, item, itemSts.sts, index)}
+                  onDragEnd={(e) => {
+                    setIsDragging(null);
+                    setDraggedElement(null)
+                  }}
+                  onMouseEnter={() => setTaskHoveredIndex(item.id)}
+                  onMouseLeave={() => setTaskHoveredIndex(null)}
+                >
+                  {taskHoveredIndex === item.id && (
+                    <span
+                      onMouseEnter={() => setDelTaskIdx(index)}
+                      onMouseLeave={() => setDelTaskIdx(null)} style={{ zIndex: '2', position: 'absolute', right: 5, top: 0 }}
+                      onClick={(e) => {
+                        setDeletingTask(item.id);
+                        setTimeout(() => {
+                          const newdataTasks = { ...dataTasks };
+                          newdataTasks[itemSts.sts].splice(index, 1);
+                          setdataTasks(newdataTasks);
+                          setDeletingTask(null);
+                        }, 500);
+                      }}
+                    >
+                      <span style={{ fontSize: 'small', fontWeight: delTaskIdx === index ? '600' : 'normal', color: 'red', cursor: 'pointer', fontSize: delTaskIdx === index ? 'medium' : 'small' }}>Delete</span>
+                    </span>
+                  )}
+                  <div style={{ marginTop: '5%' }}></div>
+                  <input className='form-control' onChange={(e) => chgTitle(e.target.value, itemSts.sts, index)} value={item.title} />
+                  <div>
+                    <span style={{ color: 'gray' }}>Due date :   </span>
 
-                  <span
-                    onClick={() => setIsOpen({ 'sts': itemSts.sts, 'id': index })}
-                    style={{ cursor: "pointer", fontSize: 'medium', fontWeight: '600', color: 'blue' }}
-                  >
-                    {format(item.duedt, "MMMM dd")}
-                  </span>
-                  <DatePicker
-                    selected={item.duedt}
-                    onChange={(date) => {
-                      const newdataTasks = { ...dataTasks };
-                      newdataTasks[itemSts.sts][index]['duedt'] = new Date(date);
-                      setdataTasks(newdataTasks);
-                      setIsOpen({});
-                    }}
-                    dateFormat="MMMM dd"
-                    open={isOpen.sts === itemSts.sts && isOpen.id === index ? true : false}
-                    onClickOutside={() => setIsOpen({})}
-                    customInput={<></>}
-                  />
+                    <span
+                      onClick={() => setIsOpen({ 'sts': itemSts.sts, 'id': index })}
+                      style={{ cursor: "pointer", fontSize: 'medium', fontWeight: '500', color: 'blue' }}
+                    >
+                      {format(item.duedt, "MMMM dd")}
+                    </span>
+                    <DatePicker
+                      selected={item.duedt}
+                      onChange={(date) => {
+                        const newdataTasks = { ...dataTasks };
+                        newdataTasks[itemSts.sts][index]['duedt'] = new Date(date);
+                        setdataTasks(newdataTasks);
+                        setIsOpen({});
+                      }}
+                      dateFormat="MMMM dd"
+                      open={isOpen.sts === itemSts.sts && isOpen.id === index ? true : false}
+                      onClickOutside={() => setIsOpen({})}
+                      customInput={<></>}
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))
+            }
             {hoveredIndex === idxSts && (
               <span style={AddTaskBtnStyle} onClick={(e) => createNewTask(e, itemSts.sts)}>+ <span style={{ fontSize: 'small' }}>add task</span> </span>
             )}
@@ -254,7 +258,7 @@ function App() {
           </div>
         )}
       </div>
-    </div>
+    </div >
   );
 }
 
